e57d84ac4af66557c2b05a23a3b1930ecc2ce710
Sachita <sachitaprusty11@gmail.com>
2018-07-07 22:43:01 +0530

    retry logic

---
+++ src/main/java/com/crm/qa/analyzer/RetryAnalyzer.java
@@ -0,0 +1,21 @@
+package com.crm.qa.analyzer;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import com.crm.qa.base.TestBase;
+
+public class RetryAnalyzer extends TestBase implements IRetryAnalyzer {
+	
+	int counter=0;
+	int retryLimit=3;
+	public boolean retry(ITestResult result) {
+		if(counter<retryLimit){
+			counter++;
+			return true;
+		}
+		
+		return false;
+	}
+
+}
